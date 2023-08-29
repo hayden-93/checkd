e1230756d22beb451c9cc0f02c230e789ed7215a
Hayden Buckley-Smith <hayden@abstack.co>
2023-08-29 19:57:09 +0100

    feat: Container component

---
+++ app/components/ui/Container.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const screenVariants = {
+  none: 'max-w-none',
+  sm: 'max-w-screen-sm',
+  md: 'max-w-screen-md',
+  lg: 'max-w-screen-lg',
+  xl: 'max-w-screen-xl',
+  full: 'max-w-full',
+} as const;
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+  id?: string;
+  screen?: 'sm' | 'md' | 'lg' | 'xl';
+}
+
+export const Container = (props: ContainerProps) => {
+  const { children, className, screen = 'none' } = props;
+
+  const classes = ['', screenVariants[screen], className].join(' ').trim();
+
+  return <div className={classes}>{children}</div>;
+};
